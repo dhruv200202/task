413930e23bcaa824ed7ad2f9d359dd4f423f8308
Dhruv <dhruv200202@gmail.com>
2023-11-14 23:29:17 +0530

    commit

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -12,7 +12,7 @@ def get_winner(player_choice, computer_choice):
     winner = "computer"
 
     if player_choice == computer_choice:
-        winner = "tie"
+        winner = "tieee"
     if player_choice == "rock" and computer_choice == "scissors":
         winner = "player"
     if player_choice == "scissors" and computer_choice == "paper":
