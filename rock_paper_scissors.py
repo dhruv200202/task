618e6bda8affb054fbac1a145b4837bfccd86583
Dhruv <dhruv200202@gmail.com>
2023-11-16 11:41:57 +0530

    comit1

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -12,7 +12,7 @@ def get_winner(player_choice, computer_choice):
     winner = "computer"
 
     if player_choice == computer_choice:
-        winner = "tieeee"
+        winner = "tie"
     if player_choice == "rock" and computer_choice == "scissors":
         winner = "player"
     if player_choice == "scissors" and computer_choice == "paper":
